a4997702fdd2baf9502a9273ceb1632a0ea340f0
Suida <suidar@foxmail.com>
2019-11-26 13:16:42 +0000

    Format files

--- src/composs/urls.py
+++ src/composs/urls.py
@@ -28,28 +28,33 @@ urlpatterns = list()
 
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="ComPoss API",
-      default_version='v0',
-      description="Hugh's Blog",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="suidar@foxmail.com"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="ComPoss API",
+        default_version='v0',
+        description="Hugh's Blog",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="suidar@foxmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 
 urlpatterns += [
-   re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-   re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-   re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
+            name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
+            name='schema-redoc'),
 ]
 
 
 urlpatterns += [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/',
+         TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
     path('api/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
